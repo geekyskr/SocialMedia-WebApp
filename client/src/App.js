e08666ee22b92961937408812aa0061557260621
Sunil <sunilrk.one@gmail.com>
2021-04-09 15:17:43 +0530

    Initial setup of project done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from './pages/Home.js';
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact render = {() => <Home />} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+
+app.listen(3001, (req, res)=>{
+  console.log("server is listening on port 3001")
+})
