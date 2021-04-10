3b5484238cfc09805d646d69275ca102076d1ab0
Sunil <sunilrk.one@gmail.com>
2021-04-10 18:09:57 +0530

    Succesfully logeed in user

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Login.css';
+import Axios from 'axios';
+
 
 function Login(){
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = ()=>{
+    Axios.post('http://localhost:3001/user/login', {username: username, password: password}).then((responce)=>{
+      console.log(responce);
+    })
+  }
   return (
-    <div>Login</div>
+    <div className="Login">
+      <h1>Login</h1>
+      <div className="LoginForm">
+        <input type="text" placeholder="username" onChange={(event)=>{
+          setUsername(event.target.value);
+        }} />
+        <input type="password" placeholder="password" onChange={(event)=>{
+          setPassword(event.target.value);
+        }} />
+        <button onClick = {login}> Login </button>
+      </div>
+    </div>
   )
 }
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -12,4 +12,23 @@ router.post('/register', (req, res, next)=>{
   })
 })
 
+router.post('/login', (req, res, next)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+  const dbquery = "select * from users where username = ?";
+  connection.query(dbquery, [username], (err, result)=>{
+    if(err){
+      console.log(err);
+    }
+    if(result.length > 0){
+      if(password == result[0].password){
+        res.json({loggedIn: true, username:username});
+      }
+      else res.json({loggedIn: false, message: "wrong password"});
+    }
+    else res.json({loggedIn: false, message: "user does not exist"});
+  })
+
+})
+
 module.exports = router;
